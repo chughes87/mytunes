262afbd0479817fc3448d742edba51da3385f2e4
Hack Reactor Students <c.hughes87@gmail.com>
2014-01-22 15:44:15 -0800

    Adds a listener on addition to collection

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,11 +2,12 @@
 var SongQueueView = Backbone.View.extend({
   initialize: function() {
     this.render();
+    this.collection.on('add', function(song) {
+      console.log(this);
+    });
   },
-  
   tagName: 'table',
   className: 'songQueueView',
-
   render: function() {
     this.$el.html('<th>Queue</th>');
     return this.$el;
