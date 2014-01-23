df7ffc5261cb876aa2f6d93ee8349b72471b3e69
Hack Reactor Students <c.hughes87@gmail.com>
2014-01-22 20:07:19 -0800

    adds listener to ended. implements play first function

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,13 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('ended', function(){
+      console.log('Ended from SongQueue');
+    })
+  },
+
+  playFirst: function(){
+    this.models[0].play();
   }
 
 });
