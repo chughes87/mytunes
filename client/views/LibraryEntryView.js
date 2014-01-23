ed85252fc616e6ef3bb2b8ad9719ddb7090c29cf
Hack Reactor Students <c.hughes87@gmail.com>
2014-01-23 15:40:12 -0800

    Adds addToPlaylist button on library entries

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -4,7 +4,9 @@ var LibraryEntryView = Backbone.View.extend({
   tagName: 'tr',
   className: 'libraryEntry',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td><%= playCount %></td>'),
+  template: _.template('<td>(<%= artist %>)</td><td>\
+                        <%= title %></td><td><%= playCount %></td>\
+                        <td><button class="addToPlaylist">Add to Playlist</buttom></td>'),
 
   events: {
     'click': function() {
