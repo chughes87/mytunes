2b620cf462a0d1f836a4ab357a6833cf42099432
Hack Reactor Students <c.hughes87@gmail.com>
2014-01-23 11:55:17 -0800

    Refactors songqueueentryview

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,7 +1,6 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
-var SongQueueEntryView = Backbone.View.extend({
+var SongQueueEntryView = LibraryEntryView.extend({
 
-  tagName: 'tr',
   className: 'queueEntry',
 
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
@@ -11,10 +10,6 @@ var SongQueueEntryView = Backbone.View.extend({
       this.model.dequeue();
       this.render();
     }
-  },
-
-  render: function(){
-    return this.$el.html(this.template(this.model.attributes));
   }
 
 });
