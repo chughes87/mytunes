bb4a43c08c8ad8b41e55fbbee7128407b93149a2
Hack Reactor Students <c.hughes87@gmail.com>
2014-01-23 16:40:52 -0800

    Adds addPlaylist function that passes playlist name to PlayLists.js

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -5,12 +5,6 @@ var AppModel = Backbone.Model.extend({
     this.set('songQueue', new SongQueue());
     this.set('playlists', new PlayLists());
 
-    /* Note that 'this' is passed as the third argument. That third argument is
-    the context. The 'play' handler will always be bound to that context we pass in.
-    In this example, we're binding it to the App. This is helpful because otherwise
-    the 'this' we use that's actually in the funciton (this.set('currentSong', song)) would
-    end up refering to the window. That's just what happens with all JS events. The handlers end up
-    getting called from the window (unless we override it, as we do here). */
     this.get('songQueue').on('play', function(song){
       this.set('currentSong', song);
     }, this);
@@ -29,7 +23,7 @@ var AppModel = Backbone.Model.extend({
     }, this);
   },
 
-  addPlaylist: function() {
-    console.log('add playlist called');
+  addPlaylist: function(name) {
+    this.get('playlists').addPlaylist(name);
   }
 });
