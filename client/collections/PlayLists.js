c0c0c8d5a51e2356afe30c920560a5817a3fd85c
Hack Reactor Students <c.hughes87@gmail.com>
2014-01-23 19:56:53 -0800

    Added PlayLists.js

--- client/collections/PlayLists.js
+++ client/collections/PlayLists.js
@@ -1,3 +1,18 @@
 var PlayLists = Backbone.Collection.extend({
+  model: Songs,
+  _currentPlayListName: 'default',
+  initialize: function(){
+  },
+  addPlaylist: function(name) {
+    var playlist = new Songs();
+    playlist.name = name;
+    // debugger;
+    this.collection.add(playlist);
+    this._currentPlayListName = name;
+  },
+  appendToCurrentPlaylist: function(song){
+    var currentPlaylist = this.collection.where({name: _currentPlayListName});
+
+  }
 
 });
